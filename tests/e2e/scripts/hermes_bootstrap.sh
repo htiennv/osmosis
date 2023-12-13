c7d12539055ec3d19de1d124f532b1118010e63c
PaddyMc <paddymchale@hotmail.com>
2023-12-13 14:23:54 +0000

    chore: edit grpc url and fix event source

--- tests/e2e/scripts/hermes_bootstrap.sh
+++ tests/e2e/scripts/hermes_bootstrap.sh
@@ -36,8 +36,8 @@ port = 3001
 [[chains]]
 id = '$OSMO_A_E2E_CHAIN_ID'
 rpc_addr = 'http://$OSMO_A_E2E_VAL_HOST:26657'
-grpc_addr = '$OSMO_A_E2E_VAL_HOST:9090'
-event_source = 'ws://$OSMO_A_E2E_VAL_HOST:26657/websocket'
+grpc_addr = 'http://$OSMO_A_E2E_VAL_HOST:9090'
+event_source = { mode = 'push', url = 'ws://$OSMO_A_E2E_VAL_HOST:26657/websocket', batch_delay = '500ms' }
 rpc_timeout = '30s'
 account_prefix = 'osmo'
 key_name = 'val01-osmosis-a'
@@ -52,8 +52,8 @@ trust_threshold = { numerator = '1', denominator = '3' }
 [[chains]]
 id = '$OSMO_B_E2E_CHAIN_ID'
 rpc_addr = 'http://$OSMO_B_E2E_VAL_HOST:26657'
-grpc_addr = '$OSMO_B_E2E_VAL_HOST:9090'
-event_source = 'ws://$OSMO_B_E2E_VAL_HOST:26657/websocket'
+grpc_addr = 'http://$OSMO_B_E2E_VAL_HOST:9090'
+event_source = { mode = 'push', url = 'ws://$OSMO_B_E2E_VAL_HOST:26657/websocket', batch_delay = '500ms' }
 rpc_timeout = '30s'
 account_prefix = 'osmo'
 key_name = 'val01-osmosis-b'
